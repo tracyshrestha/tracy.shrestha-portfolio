458adfbef2dc7cee3137c974f126a1b352a792dd
Tracy <tracyyyshrestha@gmail.com>
2025-09-14 21:52:02 +0545

    add content to projects, about and contact sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,38 @@
-import Work from "../components/Work";
+import Work from "./sections/projects/Work";
+import Skills from "./sections/Skills";
 import Intro from "../components/Introduction/Intro";
+import About from "./sections/About";
+import Education from "./sections/Education";
+import Experience from "./sections/Experience";
+// import Contact from "./sections/Contact";
 
 function Home() {
   return (
-    <div className="h-full bg-black">
-      
+    <div className="h-full bg-black w-screen">
       <div className="h-lvh">
         <Intro />
       </div>
-      <div className="">
+      <div className="py-10">
+        <About />
+      </div>
+      <div className="flex flex-col md:flex-row md:space-x-6">
+        <div className="py-10">
+          <Education />
+        </div>
+        <div className="py-10">
+          <Experience />
+        </div>
+      </div>
+
+      <div className="py-10">
+        <Skills />
+      </div>
+      <div className="py-10">
         <Work />
       </div>
+      {/* <div className="py-10">
+        <Contact />
+      </div> */}
     </div>
   );
 }
